6b4805cb0ea23791c81e0a734e5d49bad00dbb86
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-05-30 14:52:55 +0100

    database config

--- packages/core/src/config/database.js
+++ packages/core/src/config/database.js
@@ -1,19 +1,19 @@
-// import { resolve } from 'path';
-
-// export default {
-//   client: 'sqlite3',
-//   connection: () => ({
-//     filename: resolve('src', 'database', process.env.SQLITE_FILENAME),
-//   }),
-//   useNullAsDefault: true,
-// };
+import { resolve } from 'path';
 
 export default {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: process.env.PASSWORD,
-    database: 'dbm',
-  },
+  client: 'sqlite3',
+  connection: () => ({
+    filename: resolve('src', 'database', process.env.SQLITE_FILENAME),
+  }),
+  useNullAsDefault: true,
 };
+
+// export default {
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'postgres',
+//     password: process.env.PASSWORD,
+//     database: 'dbm',
+//   },
+// };
