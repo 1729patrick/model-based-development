d0e416868bc97bc6d7436b791e3a8acf6244e088
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-07-03 21:31:16 +0100

    paths

--- packages/core/src/config/path.js
+++ packages/core/src/config/path.js
@@ -4,3 +4,4 @@ export const VIEW_PATH = resolve('..', 'front', 'src');
 export const ROUTER_PATH = resolve('src', 'routes', 'app');
 export const MODEL_PATH = resolve('src', 'app', 'models');
 export const CONTROLLER_PATH = resolve('src', 'app', 'controllers');
+export const HOME_PATH = resolve('..', 'front', 'src', 'pages', 'Home');
